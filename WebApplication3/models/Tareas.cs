7e8558ac4ef715177117f1a51b0577764c33211b
Jhomayra21 <jhomayramamanichoqueticlla21@gmail.com>
2025-08-19 20:47:04 -0400

    implementar_json

---
+++ WebApplication3/models/Tareas.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication3.models
+{
+    public class Tareas
+    {
+        public string idTarea { get; set; }
+        public string nombreTarea { get; set; }
+        public string fechaVencimiento { get; set; }
+        public string estado { get; set; }
+    }
+}
